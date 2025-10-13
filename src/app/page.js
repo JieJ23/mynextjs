da2ddfeafbea4e3ce2dbd39aa150e1f425fc0c12
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-10-13 15:30:16 -0400

    Testing cache

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,21 @@ import Box1 from "@/components/Box1";
 
 export default async function Home() {
 
-  const data = await fetch('https://api.vercel.app/blog')
+  const data = await fetch('https://api.vercel.app/blog', {
+    next: { revalidate: 30 }
+  })
   const posts = await data.json()
+  const fetchedAt = new Date().toISOString();
+
 
   return (
     <div className="flex items-center justify-items-center p-4">
       <main >
 
         <Link href="/About" className="font-[ubuntu] text-[20px]">Go to About Page</Link>
+
+        <p>Data fetched at: {fetchedAt}</p>
+
 
         <div className="font-[ubuntu] text-[16px] my-4">
           <Box1 data={posts} />

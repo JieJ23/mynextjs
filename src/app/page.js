885b8c75e8db2f367d179be79be7db9e3734c71f
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-10-13 16:09:23 -0400

    Revalidate

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,9 @@ import Box1 from "@/components/Box1";
 
 export default async function Home() {
 
-  const data = await fetch('https://api.vercel.app/blog')
+  const data = await fetch('https://api.vercel.app/blog', {
+    next: { revalidate: 10 }
+  })
   const posts = await data.json()
   const fetchedAt = new Date().toISOString();
 

498ecd61e18e72d9727a72df33ac4df6be997d99
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-10-16 12:27:04 -0400

    Testing Youtube Search API

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 const revalidate = 43200;
 
-import { youtube_ids } from "@/data/YTid";
+// import { youtube_ids } from "@/data/YTid";
 import Standard from "@/components/Standard";
 
 const YOUTUBE_API_KEY = `AIzaSyBdA6aG9KHhqJ_cKhnHXkVzKNWsWZb2F3U`
@@ -14,27 +14,35 @@ function chunkArray(array, chunkSize) {
 }
 
 export default async function Home() {
-  const chunks = chunkArray(youtube_ids, 50);
+  // const chunks = chunkArray(youtube_ids, 50);
 
-  const allPosts = [];
+  // const allPosts = [];
 
-  for (const chunk of chunks) {
-    const id_strings = chunk.join(",");
-    const res = await fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${id_strings}&key=${YOUTUBE_API_KEY}`,
-      { next: { revalidate: 43200 } }
-    );
-    const data = await res.json();
-    allPosts.push(...data.items); // merge items into allPosts
-  }
+  // for (const chunk of chunks) {
+  //   const id_strings = chunk.join(",");
+  //   const res = await fetch(
+  //     `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${id_strings}&key=${YOUTUBE_API_KEY}`,
+  //     { next: { revalidate: 43200 } }
+  //   );
+  //   const data = await res.json();
+  //   allPosts.push(...data.items); // merge items into allPosts
+  // }
 
+  const res = await fetch(
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=hades+fear&type=video&order=date&maxResults=50&key=${YOUTUBE_API_KEY}`,
+    { next: { revalidate: 43200 } }
+  );
+
+  const data = await res.json();
   const fetchedAt = new Date().toTimeString();
+
+
 
   return (
     <div>
       <main className="text-white">
         <p className="text-[14px] p-2">Data fetched at: {fetchedAt}</p>
-        <Standard rawData={allPosts} />
+        <Standard rawData={data.items} />
       </main>
     </div>
   );
--- src/components/Standard.jsx
+++ src/components/Standard.jsx
@@ -1,18 +1,13 @@
 "use cilent";
 
 export default function Standard({ rawData }) {
-  const sortDataByDate = rawData.sort(
-    (a, b) => new Date(b.snippet.publishedAt.slice(0, 10)) - new Date(a.snippet.publishedAt.slice(0, 10))
-  );
-
-  console.log(rawData[0]);
   return (
-    <div className="my-4 max-w-[1400px] mx-auto">
-      <div className="p-2 grid grid-cols-2 md:grid-cols-4 gap-2 text-[12px] md:text-[13px]">
-        {sortDataByDate.map((obj, index) => (
+    <div className="my-4 max-w-[1600px] mx-auto font-[font]">
+      <div className="p-2 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 text-[12px] md:text-[13px]">
+        {rawData.map((obj, index) => (
           <div key={index} className="rounded">
             <div className="aspect-video border-1 border-[#28282b] rounded-lg">
-              <a href={`https://www.youtube.com/watch?v=${obj.id}`} target="_blank">
+              <a href={`https://www.youtube.com/watch?v=${obj.id.videoId}`} target="_blank">
                 <img
                   src={`${obj.snippet.thumbnails.medium.url}`}
                   alt="Thumbnails"
@@ -22,14 +17,11 @@ export default function Standard({ rawData }) {
             </div>
             <div className="px-2">
               <div className="line-clamp-2">{obj.snippet.title}</div>
-              <div className="flex justify-between">
+              <div className="flex justify-between text-[12px] my-0.5">
                 <div>{obj.snippet.channelTitle}</div>
-                <div>Like: {obj.statistics.likeCount}</div>
-              </div>
-              <div className="flex justify-between">
                 <div>{obj.snippet.publishedAt.slice(0, 10)}</div>
-                <div>View: {obj.statistics.viewCount}</div>
               </div>
+              <div className="line-clamp-2">{obj.snippet.description}</div>
             </div>
           </div>
         ))}

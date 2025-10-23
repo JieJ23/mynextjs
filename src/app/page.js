90ba70da5af56b9ef93a46e6ad5b21debe33bcc0
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-10-23 09:36:31 -0400

    Testing default SSR

--- src/app/page.js
+++ src/app/page.js
@@ -1,48 +1,21 @@
-const revalidate = 43200;
-
-// import { youtube_ids } from "@/data/YTid";
-import Standard from "@/components/Standard";
-
-const YOUTUBE_API_KEY = `AIzaSyBdA6aG9KHhqJ_cKhnHXkVzKNWsWZb2F3U`
-
-function chunkArray(array, chunkSize) {
-  const chunks = [];
-  for (let i = 0; i < array.length; i += chunkSize) {
-    chunks.push(array.slice(i, i + chunkSize));
-  }
-  return chunks;
-}
-
 export default async function Home() {
-  // const chunks = chunkArray(youtube_ids, 50);
-
-  // const allPosts = [];
-
-  // for (const chunk of chunks) {
-  //   const id_strings = chunk.join(",");
-  //   const res = await fetch(
-  //     `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${id_strings}&key=${YOUTUBE_API_KEY}`,
-  //     { next: { revalidate: 43200 } }
-  //   );
-  //   const data = await res.json();
-  //   allPosts.push(...data.items); // merge items into allPosts
-  // }
 
   const res = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=hades+fear&type=video&order=date&maxResults=50&key=${YOUTUBE_API_KEY}`,
-    { next: { revalidate: 43200 } }
+    `https://script.google.com/macros/s/AKfycbw1iX5AdD32YcXYohQLAgEx3SDPTsH3V41RGCGts2bNk3-yYfSlEpZgufNt-G60gc7t/exec`
   );
-
   const data = await res.json();
-  const fetchedAt = new Date().toTimeString();
-
 
 
   return (
     <div>
       <main className="text-white">
-        <p className="text-[14px] p-2">Data fetched at: {fetchedAt}</p>
-        <Standard rawData={data.items} />
+        <div>
+          {data.map((obj, index) => (
+            <div key={index}>
+              {obj.nam} - {index}
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );

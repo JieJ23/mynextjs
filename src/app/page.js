19a1dab185bd6460b38222f7a1710a7b4e58b43b
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-10-23 09:46:19 -0400

    Added revalidation

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default async function Home() {
 
   const res = await fetch(
-    `https://script.google.com/macros/s/AKfycbw1iX5AdD32YcXYohQLAgEx3SDPTsH3V41RGCGts2bNk3-yYfSlEpZgufNt-G60gc7t/exec`, { cache: 'no-store' }
+    `https://script.google.com/macros/s/AKfycbw1iX5AdD32YcXYohQLAgEx3SDPTsH3V41RGCGts2bNk3-yYfSlEpZgufNt-G60gc7t/exec`, { next: { revalidate: 30 } }
   );
   const data = await res.json();
 

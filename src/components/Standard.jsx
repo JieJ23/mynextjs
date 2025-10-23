82def04c470b259166d76de34d6bb4332c29bcb9
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-10-23 09:54:39 -0400

    Added Testing

--- src/components/Standard.jsx
+++ src/components/Standard.jsx
@@ -1,13 +1,29 @@
 "use client";
 
 export default function Standard({ data }) {
+  const high = data.filter((obj) => obj.fea > 60);
+
   return (
     <div>
-      {data.map((obj, index) => (
-        <div key={index}>
-          {obj.nam} - {index}
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eum eius facere delectus amet quos dolores
+        deleniti odit tempora totam? Tempore quo optio cupiditate non. Doloribus quo unde, soluta esse eaque architecto.
+        Fuga, consequuntur ea itaque quos facere facilis corporis? Libero saepe eveniet corporis assumenda impedit
+        laudantium nemo mollitia incidunt?
+      </div>
+      {high.map((obj, index) => (
+        <div key={index} className="flex gap-2">
+          <div>{obj.nam}</div>
+          <div>{obj.fea}</div>
+          <div>{obj.aso}</div>
         </div>
       ))}
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eum eius facere delectus amet quos dolores
+        deleniti odit tempora totam? Tempore quo optio cupiditate non. Doloribus quo unde, soluta esse eaque architecto.
+        Fuga, consequuntur ea itaque quos facere facilis corporis? Libero saepe eveniet corporis assumenda impedit
+        laudantium nemo mollitia incidunt?
+      </div>
     </div>
   );
 }
